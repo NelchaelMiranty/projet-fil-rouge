10361108fd6cb29b22d6b38efa7c67b937c25092
NelchaelMiranty <mirantyrahaingomaminiaina@gmail.com>
2025-09-16 11:33:58 +0300

    #

--- todoApp/src/app/home/home.page.ts
+++ todoApp/src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, FormsModule, CommonModule],
 })
 export class HomePage {
+  task = '';
+  tasks: string[] = [];
+
   constructor() {}
+
+  addTask() {
+    if (this.task.trim()) {
+      this.tasks.push(this.task.trim());
+      this.task = '';
+    }
+  }
 }
